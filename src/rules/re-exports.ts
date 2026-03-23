31c5d26bb79261a3e84f615b6c8ea8e4d9b85cdd
mantoci <matteo.antoci@lastminute.com>
2026-03-23 11:47:29 +0100

    chore: bump version to 2.2.1 and remove canonical/no-re-export rule from re-exports config

--- src/rules/re-exports.ts
+++ src/rules/re-exports.ts
@@ -1,7 +1,6 @@
 import type { Linter } from "eslint";
 
 export const createReExportRules = (): Linter.RulesRecord => ({
-  "canonical/no-re-export": "error",
   "canonical/no-export-all": "error",
   "canonical/filename-no-index": "error",
 });
